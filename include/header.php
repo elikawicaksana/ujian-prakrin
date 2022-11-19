b1ea761252b0f100855e47cd94f4ccfb12647fda
Elika Wicaksana <112156728+elikawicaksana@users.noreply.github.com>
2022-11-19 22:57:03 +0800

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,28 @@
+<!-- ======= Header ======= -->
+
+<header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+        <h1 class="logo me-auto"><a href="index.php">Elika</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                <li><a class="nav-link scrollto" href="#about">About</a></li>
+                <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
+                <li class="dropdown"><a href="#"><span>Attachment</span> <i class="bi bi-chevron-down"></i></a>
+                <ul>
+                    <li><a href="jurnal.php">Jurnal</a></li>
+                    <li><a href="absensi.php">Absensi</a></li>
+                    <li><a href="nilai.php">Nilai Kompetensi</a></li>
+                    <li><a href="surat.php">Surat Balasan</a></li>
+                    <li><a href="bimbingan.php">Form Bimbingan</a></li>
+                    <li><a href="sertifikat.php">Sertifikat</a></li>
+                </ul>
+                </li>
+                <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+    </div>
+</header>
---
+++ include/script.php
@@ -0,0 +1,11 @@
+<!-- Vendor JS Files -->
+<script src="Arsha/assets/vendor/aos/aos.js"></script>
+<script src="Arsha/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="Arsha/assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="Arsha/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="Arsha/assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="Arsha/assets/vendor/waypoints/noframework.waypoints.js"></script>
+<script src="Arsha/assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="Arsha/assets/js/main.js"></script>
